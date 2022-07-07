8f9be972a54b6b353ea6b33592efd03e632e4513
Noir <chenDSEE@163.com>
2022-07-07 22:37:19 +0800

    add timeout control for handler

--- main.go
+++ main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"MarineSnow/framework"
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func helloHandler(ctx *framework.Context) error {
 	return nil
 }
 
+type info struct {
+	name string
+	data string
+}
+
 func timedemoHandler(ctx *framework.Context) error {
+	infoChan := make(chan *info, 1)
+
+	timeoutCtx, cancel := context.WithTimeout(ctx.BaseContext(), time.Duration(2*time.Second))
+	defer cancel()
+
+	go func() {
+		fmt.Printf("RPC start  -->\n")
+
+		time.Sleep(1 * time.Second)
+		infoChan <- &info{name: "name", data: "data"}
+
+		fmt.Printf("RPC end   <--\n")
+	}()
+
+	select {
+	case info := <-infoChan:
+		fmt.Printf("get information:[name:%s, data:%s]\n", info.name, info.data)
+	case <-timeoutCtx.Done():
+		fmt.Printf("timeout and cancel this RPC\n")
+	}
+
+	fmt.Printf("timedemoHandler() finish and exit\n")
 	return nil
 }
 
@@ -26,6 +55,7 @@ func main() {
 
 	/* register HTTP handler and route */
 	core.RegisterHandlerFunc("/hello", helloHandler)
+	core.RegisterHandlerFunc("/timeout", timedemoHandler)
 
 	_ = server.ListenAndServe() // 依然借助 http.Server 来启动 http 监听、处理 connect
 
